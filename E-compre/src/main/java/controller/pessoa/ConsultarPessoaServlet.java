f21b50c9bc9eac7f240d0c86b64784d5e124f29b
Nathan Oliveira de Santana <oliveira.nathan@aluno.ifsp.edu.br>
2022-06-22 00:19:32 -0300

    Primeiros arquivos do Controller e View adicionados.

---
+++ E-compre/src/main/java/controller/pessoa/ConsultarPessoaServlet.java
@@ -0,0 +1,33 @@
+package controller.pessoa;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class ConsultarPessoaServlet
+ */
+@WebServlet({"/pessoa/consultar","/admin/consultar","/user/consultar"})
+public class ConsultarPessoaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConsultarPessoaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
